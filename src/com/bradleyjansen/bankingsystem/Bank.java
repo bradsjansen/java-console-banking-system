888f93ab29d66dfe831a33da69fd11e7e430a70c
bradsjansen <113173037+bradsjansen@users.noreply.github.com>
2026-03-01 19:39:45 +0200

    Add bank class skeleton with account management method stubs

--- src/com/bradleyjansen/bankingsystem/Bank.java
+++ src/com/bradleyjansen/bankingsystem/Bank.java
@@ -1,6 +1,30 @@
 package com.bradleyjansen.bankingsystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
 
+    private List<Account> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public void createAccount(String accountNumber) {
+
+    }
+
+    public Account getAccount(String accountNumber) {
+        return null;
+    }
+
+    public void deposit(String accountNumber, double amount) {
+
+    }
+
+    public void withdraw(String accountNumber, double amount) {
+
+    }
 
 }
